9ca606cb88592c329842e24044efa40964686149
tuantien <5451074016@st.utc2.edu.vn>
2017-04-10 13:44:57 +0700

    V7
    Có phân quyền

---
+++ CiOneElearning/CiOneElearning/Areas/Admin/Controllers/ManageRoleController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CiOneElearning.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+namespace CiOneElearning.Areas.Admin.Controllers
+{
+    public class ManageRoleController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        // GET: Admin/ManageRole
+        public ActionResult Index()
+        {
+            var model = context.Roles.AsEnumerable();
+            return View(model);
+        }
+        public ViewResult Create()
+
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(IdentityRole role)
+
+        {
+
+            try
+
+            {
+
+                if (ModelState.IsValid)
+
+                {
+
+                    context.Roles.Add(role);
+
+                    context.SaveChanges();
+
+                }
+
+                return RedirectToAction("Index");
+
+            }
+
+            catch(Exception ex)
+
+            {
+
+                ModelState.AddModelError("", ex.Message);
+
+            }
+
+            return View(role);
+
+        }
+        public ActionResult Delete(string Id)
+        {
+
+            var model = context.Roles.Find(Id);
+
+            return View(model);
+
+        }
+
+
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+
+        [ActionName("Delete")]
+
+        public ActionResult DeleteConfirmed(string Id)
+        {
+
+            IdentityRole model = null;
+
+            try
+            {
+
+                model = context.Roles.Find(Id);
+
+                context.Roles.Remove(model);
+
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+
+            catch (Exception ex)
+            {
+
+                ModelState.AddModelError("", ex.Message);
+
+            }
+
+            return View(model);
+
+        }
+    }
+}
